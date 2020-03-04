c54657fc05188447aec868d7c525941988bfe742
John Robertson <john.robertson@p97.com>
2020-03-04 09:12:14 -0600

    Added an almost purely skeleton solution, containing a test project.

---
+++ Atlas/Atlas.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Atlas.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Succeed() => new Class1().Succeed();
+
+        [TestMethod]
+        public void Fail() => new Class1().Fail();
+    }
+}
---
+++ Atlas/Atlas/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Atlas
+{
+    public class Class1
+    {
+        public void Succeed() { }
+
+        public void Fail() => throw new Exception("Test Failure");
+    }
+}
